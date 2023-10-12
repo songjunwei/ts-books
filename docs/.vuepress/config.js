88d9f69bdf936336021a54aa18e05c425976795b
songjunwei <songjunwei@chinamobile.com>
2023-10-12 14:38:46 +0800

    init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    base: '/ts-books/',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: 'ts books', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/songjunwei' },
+                ]
+            }
+        ]
+    }
+  }
